21e00a6c9c31d69e69b4a7bf062e9a7858c2389f
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-01 15:40:05 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "hello",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<Style lang="scss">
+body {
+  h1 {
+    color: red;
+  }
+}
+</Style>
